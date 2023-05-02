1feef40dc3c06ae57fc41c67620e93552cfb0d71
Anatoliy Tokarev <tokarev182@yandex.ru>
2023-05-02 20:38:21 +0300

    переход по карточкам с заходом в карточки и сменой страниц

--- parser.js
+++ parser.js
@@ -1,56 +1,113 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-const catalog = [];
+const products = [];
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 100,
+    // slowMo: 50,
   });
   const page = await browser.newPage();
 
-  //?PAGEN_1=13
-
   await page.goto(
-    "https://dostavka.dixy.ru/catalog/ovoshchi_frukty_zelen_griby/",
+    `https://dostavka.dixy.ru/catalog/ovoshchi_frukty_zelen_griby/`,
     { waiiUntil: "domcontentloaded" }
   );
   const arrowNext = (await page.$(".flex-next")) !== null; //на последнй странице стрелка будет false
 
+  ///пока существует стрелка далее
   while (arrowNext) {
-    await page.waitForSelector("div.inner_wrapper", {
-      waiiUntil: "domcontentloaded",
-    });
-    const products = await page.$$(
-      "div.inner_wrapper > div.ajax_load.cur.block > div.top_wrapper.items_wrapper.catalog_block_template > .items > .item"
-    );
-    await page.waitForSelector(".main-catalog-wrapper", {
-      waiiUntil: "domcontentloaded",
+    //загружаем каталог карточек на странице
+    await page.waitForSelector("div.inner_wrapper");
+
+    //делаем список карточек с линками на переход внутрь
+    let cards = await page.evaluate(() => {
+      let links = Array.from(document.querySelectorAll(".card-link")).map(
+        (link) => link.href
+      );
+      return { links };
     });
 
-    for (product of products) {
-      const item = {};
-        const title = await page.evaluate(
-          (el) => el.querySelector(".card-name").innerText,
-          product
+    for (let i = 0; i < cards.links.length; i++) {
+      await page.goto(cards.links[i]);
+
+      await page.waitForSelector(".detail-page");
+
+      const product = {};
+
+      try {
+        //заголок
+        await page.waitForSelector("div.product-title > span");
+        const title = await page.$eval("div.product-title > span", (elem) => {
+          return elem.innerHTML;
+        });
+        product.title = title;
+      } catch (error) {}
+
+      try {
+        //артикул
+        await page.waitForSelector("div.top-info > span");
+        const article = await page.$eval("div.top-info > span", (elem) => {
+          return elem.innerHTML.slice(10, -1);
+        });
+        product.article = article;
+      } catch (error) {}
+
+      try {
+        await page.waitForSelector(".product-gallery");
+        const picture = await page.$eval(
+          "#slick-slide00 > div > div > img",
+          (elem) => {
+            return elem.src;
+          }
         );
-    //   const title = await page.$eval(".card-name", (elem) => {
-    //     return elem.innerText;
-    //   });
+        product.picture = picture;
+      } catch (error) {}
 
-      item.title = title;
-      catalog.push(item);
+      try {
+        //страна
+        await page.waitForSelector(
+          "div.property-list > div:nth-child(2) > div.prop-value"
+        );
+        const country = await page.$eval(
+          "div.property-list > div:nth-child(2) > div.prop-value",
+          (elem) => {
+            return elem.innerText;
+          }
+        );
+        product.country = country;
+      } catch (error) {}
 
-      fs.writeFile("products.txt", JSON.stringify(catalog), function (err) {
+      try {
+        await page.waitForSelector(
+          "div.property-list > div:nth-child(3) > div.prop-value"
+        );
+        const type = await page.$eval(
+          "div.property-list > div:nth-child(3) > div.prop-value",
+          (elem) => {
+            return elem.innerText;
+          }
+        );
+        product.type = type;
+      } catch (error) {}
+
+      products.push(product);
+
+      fs.writeFile("products.js", JSON.stringify(products), function (err) {
         if (err) throw err;
+        console.log("Products saved to products.txt!");
       });
+
+      await page.click("#bx_breadcrumb_2 > a > span");
     }
 
     await page.waitForSelector(".flex-next");
-    await page.click(".flex-next", {
-      timeout: 3000,
-    });
+
+    if (arrowNext) {
+      await page.click(".flex-next");
+    } else {
+      await browser.close();
+    }
   }
-  await browser.close();
 })();
